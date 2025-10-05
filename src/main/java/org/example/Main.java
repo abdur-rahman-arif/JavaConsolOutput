0bcd6c9c197d27ffc518167367c6c416b79bd14f
Abdur Rahman <abrahmanarif.it@gmail.com>
2025-10-05 12:57:32 +0600

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Name:");
+        String name = input.nextLine();
+        System.out.println(name);
+        System.out.println("Java is a reliable, secure, and fast language that runs on any platform, has high demand with great job opportunities, and offers a huge community with plenty of learning resources.");
+        System.out.println("To begin my career as a Java Developer, applying my Java programming knowledge to create efficient and user-friendly applications while gaining practical experience from real-world projects.");
+
+    }
+}
